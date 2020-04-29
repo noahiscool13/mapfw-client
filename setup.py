c7c0e949a6d41f4ab45327488e815e69a388263d
njadoenathmisi <n.j.m.jadoenathmisier@student.tudelft.nl>
2020-04-29 11:40:56 +0200

    Add files for pypi package

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import os
+
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(name='mapfw',
       version='0.1',
       description='API client for mapfw challenges',
-      url='http://github.com/',
+      url='https://github.com/noahiscool13/mapfw-client',
       author='Noah Jadoenathmisier',
       author_email='n.j.m.jadoenathmisier@student.tudelft.nl',
       license='MIT',
-      packages=['mapfw']
+      packages=['mapfw'],
+      long_description=read('README.md'),
+      zip_safe=False,
+      install_requires=[
+          'requests'
+      ],
       )
